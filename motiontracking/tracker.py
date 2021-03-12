adc9b35dc7969aed70da357a2b0dcb16309e5970
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2021-03-12 16:47:52 -0700

    remove optimizations for now

--- motiontracking/tracker.py
+++ motiontracking/tracker.py
@@ -40,8 +40,8 @@ class Tracker:
 		self.particles_init = self.particles.copy()
 		self.ref_index = np.linspace(0,len(self.particles_init)-1,len(self.particles_init)).astype(int)
 		self.reference_points = self.refscan[self.refscan.query(self.motionmodel.xy,10),:]
-		l = 0.2
-		sigma2 = 0.02 #Observational Variance
+		l = 0.1
+		sigma2 = 0.01 #Observational Variance
 		self.ref_mean = self.reference_points.mean(axis=0)
 		self.ref_std = self.reference_points.std(axis=0)
 		self.x_train = (self.reference_points - self.ref_mean)/self.ref_std
@@ -55,7 +55,7 @@ class Tracker:
 		"Weighted Particle Mean [x, y, z, vx, vy, vz]"
 		return np.average(self.particles, weights=self.weights, axis=0)		
 		
-
+	'''
 	def pointset_to_distmat(self,scan,pointset):
 		start = time.time()
 		pointset = np.array(pointset) 
@@ -68,12 +68,11 @@ class Tracker:
 		mat = sparsemat((distances,(pointsetinds,pointsetinds)),shape=(maxind,maxind) )
 		print(f"\n KSS Took {time.time()-start} Seconds For {mat.shape} Matrix")
 		return mat
-
-
-	def process_points(self,scan,cloudset:list,pointset: set,particles):
-		cloudset.append(scan.query(particles,radius=10))
-		pointset |= set(cloudset[-1])
-
+'''
+	
+	def process_points(self,scan,point):
+		return scan.query(point,radius=10)
+		#pointset |= set(cloudset[-1])
 
 	def scans_likelihood(self, scan: Scan,dt: datetime.timedelta=None):
 		l = 0.1
@@ -87,43 +86,42 @@ class Tracker:
 	
 		self.datetime = scan.datetime
 		self.particles = self.motionmodel.evolve_particles(self.particles,self.dt)
-		#delta_0 = self.particles_init[self.ref_index,:2] - self.motionmodel.xy
-		#delta_p = self.particles[:,:3] - self.particles_init[self.ref_index,:3] 
+		delta_0 = self.particles_init[self.ref_index,:2] - self.motionmodel.xy
+		delta_p = self.particles[:,:3] - self.particles_init[self.ref_index,:3] 
 		# Will try and normalize by query point; using this approach precludes the use of precomputed values
 		
-		pointset = set()
-		testclouds = []
+		#pointset = set()
+	
 		start = time.time()
-		[self.process_points(scan,testclouds,pointset,particle) for particle in list(self.particles[:,:2])]
+		testclouds = [scan.query(point,radius=10) for particle in list(self.particles[:,:2])]
 		print(f"\n Test Clouds Queried in {time.time()-start} Seconds\n")
-		pointset = list(pointset)
-		KSS = self.pointset_to_distmat(scan,pointset)
-		x_test = scan.points[np.array(pointset)]
-		x_test = (x_test - self.ref_mean)/self.ref_std
+		#KSS = self.pointset_to_distmat(scan,pointset)
+		#x_test = scan.points[np.array(pointset)]
+		#x_test = (x_test - self.ref_mean)/self.ref_std
 
-		KSTAR = np.exp(-dist(x_test[:,:2],self.x_train[:,:2],squared=True)/l**2).flatten()
-		rows = np.tile(pointset,(self.x_train.shape[0]))
-		cols = np.tile(np.arange(self.x_train.shape[0]),len(pointset))
-		KSTAR = sparsemat((KSTAR,(rows,cols)),shape=(max(pointset)+1,self.x_train.shape[0]))
+		#KSTAR = np.exp(-dist(x_test[:,:2],self.x_train[:,:2],squared=True)/l**2).flatten()
+		#rows = np.tile(pointset,(self.x_train.shape[0]))
+		#cols = np.tile(np.arange(self.x_train.shape[0]),len(pointset))
+		#KSTAR = sparsemat((KSTAR,(rows,cols)),shape=(max(pointset)+1,self.x_train.shape[0]))
 
 		particle_loglike = []
 		start = time.time()
 		for testcloud in testclouds:
 			looptime = time.time()
-			inds = np.array([i for i in testcloud])
-			A = time.time() 
-			Kss = KSS[inds,inds]
-			B = time.time() 
-			Kstar = KSTAR[inds,:]
-			C = time.time()
-			xtestinds = np.array([pointset.index(i) for i in testcloud])
-			x_test_sub = x_test[xtestinds,:]
+			x_test = scan.points[np.array(testcloud)]
+			x_test = (x_test - self.ref_mean)/self.ref_std
+
+			Kss = np.exp(-dist(self.x_test[:,:2],self.x_test[:,:2],squared=True)/l**2)
+			Kstar = np.exp(-dist(self.x_train[:,:2],self.x_test[:,:2],squared=True)/l**2)
+
+
+		
 			mu = Kstar @ self.premu
 			Sigma = Kss - Kstar @ self.Kinv @ Kstar.T + sigma2*np.eye(Kss.shape[0])
-			log_like = -0.5*(np.log(2*np.pi)*inds.shape[0] ) + np.linalg.slogdet(Sigma)[1] +0.5*(x_test_sub[:,2]-mu)@np.linalg.inv(Sigma)@(x_test_sub[:,2]-mu).T
+			log_like = -0.5*(np.log(2*np.pi)*inds.shape[0] ) + np.linalg.slogdet(Sigma)[1] +0.5*(x_test[:,2]-mu)@np.linalg.inv(Sigma)@(x_test[:,2]-mu).T
 			particle_loglike.append(log_like)
 			D = time.time()
-			print(f" {B-A} \n{C-B}\n {D-C}\n{D-looptime}\n")
+			print(f"\n Loop Time {D-looptime}\n")
 		print(f"\n Likelihoods Took {time.time()-start} Seconds \n")
 		particle_loglike = np.array(particle_loglike)
 		w = np.exp(0.2*(particle_loglike-particle_loglike.max())) + 1e-300
